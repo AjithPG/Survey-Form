cbe2ecb092eefe68ee04a7684751b7f01c73411b
AjithPG <ajithpg008@gmail.com>
2019-08-26 22:34:04 +0530

    :rocket: Set new PHP page

---
+++ form.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Your Name:<?php echo $_POST["name"]; ?><br>
+Your email address: <?php echo $_POST["e-mail"]; ?><br>
+Your Age:<?php echo $_POST["age"]; ?><br>
+Current Status:<?php echo $_POST["currentstatus"]; ?><br>
+Gender:<?php echo $_POST["gender"]; ?><br>
+Educational Qualification:<?php echo $_POST["Degree"]; ?><br>
+Comments:<?php echo $_POST["suggestion"]; ?><br>
+</body>
+</html>
